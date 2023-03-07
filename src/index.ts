ff3063ea1a3b8758ffe10213746575e8d9a6f6f0
Lang Ming <langming.qzl@alibaba-inc.com>
2023-03-07 09:53:21 +0800

    feat: add proxy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "./lib/db";
 import express from "express";
+import proxy from 'express-http-proxy'
 import countryRoutes from "./routes/country";
 
 const app = express();
@@ -8,6 +9,19 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+app.use(proxy('/proxy', {
+  proxyReqPathResolver: (req: Request) => {
+    if (/^https?:\/\/[^/]+\/proxy\/(\w+)\/(.*)$/.test(req.url)) {
+      const proxy = RegExp.$1
+      const rest = RegExp.$2
+      if (proxy === 'openai') {
+        return `https://api.openai.com/${rest}`
+      }
+    }
+
+    return ''
+  }
+}))
 
 app.get("/", async (req, res) => {
   res.json({ message: "Please visit /countries to view all the countries" });
