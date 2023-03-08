5207c3ceb9d1a56f893b7c15f0ccef4177580613
Lang Ming <langming.qzl@alibaba-inc.com>
2023-03-08 08:18:46 +0800

    feat: proxy

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,28 @@
-import "./lib/db";
-import express from "express";
-import proxy from 'express-http-proxy'
-import countryRoutes from "./routes/country";
+// import './lib/db'
+import express from 'express'
+import httpProxy from 'http-proxy'
+// import countryRoutes from './routes/country'
 
-const app = express();
-const port = process.env.PORT || 3333;
+const app = express()
+const proxy = httpProxy.createProxyServer()
+const port = process.env.PORT || 3333
 
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
-app.use(proxy('/proxy', {
-  proxyReqPathResolver: (req: Request) => {
-    if (/^https?:\/\/[^/]+\/proxy\/(\w+)\/(.*)$/.test(req.url)) {
-      const proxy = RegExp.$1
-      const rest = RegExp.$2
-      if (proxy === 'openai') {
-        return `https://api.openai.com/${rest}`
-      }
-    }
+// app.use(express.json())
+// app.use(express.raw({ type: 'application/vnd.custom-type' }))
+// app.use(express.text({ type: 'text/html' }))
+app.use('/proxy/openai', (req, res) => {
+  console.log(req.url)
+  proxy.web(req, res, {
+    target: 'https://api.openai.com',
+    changeOrigin: true
+  })
+})
+app.get('/', async (req, res) => {
+  res.send({ message: 'Please visit /countries to view all the countries' })
+})
 
-    return ''
-  }
-}))
-
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
-});
-
-app.use("/countries", countryRoutes);
+// app.use('/countries', countryRoutes)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
